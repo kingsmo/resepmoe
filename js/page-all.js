01c1068093b14986027992cae8184c8bf652d2df
chalif <chalifnurfaizi6@gmail.com>
2019-11-09 18:49:15 +0700

    Update page-all.js

--- js/page-all.js
+++ js/page-all.js
@@ -1,17 +1,9 @@
     $(function(){
       var layout = 'https://resepmoe.s3.us-east-2.amazonaws.com/views/layout/';
-      var guest = 'https://resepmoe.s3.us-east-2.amazonaws.com/views/guest/';
-      var member = 'https://resepmoe.s3.us-east-2.amazonaws.com/views/member/';
       
       var header = $('[data-header]');
       jQuery.each(header, function(){
         var file = layout + $(this).data('header') + '.html';
-        $(this).load(file);
-      });
-
-      var body = $('[data-body]');
-      jQuery.each(body, function(){
-        var file = guest + $(this).data('body') + '.html';
         $(this).load(file);
       });
 
