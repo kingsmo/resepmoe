755a9d2a1345a575cffe0a85ea8f3c6f4b8ee2a3
chalif <chalifnurfaizi6@gmail.com>
2019-11-09 13:01:25 +0700

    commit

--- js/page-all.js
+++ js/page-all.js
@@ -2,16 +2,19 @@
       var layout = './views/layout/';
       var guest = './views/guest/';
       var member = './views/member/';
+      
       var header = $('[data-header]');
       jQuery.each(header, function(){
         var file = layout + $(this).data('header') + '.html';
         $(this).load(file);
       });
+
       var body = $('[data-body]');
       jQuery.each(body, function(){
         var file = guest + $(this).data('body') + '.html';
         $(this).load(file);
       });
+
       var footer = $('[data-footer]');
       jQuery.each(footer, function(){
         var file = layout + $(this).data('footer') + '.html';
