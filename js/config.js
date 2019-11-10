4a3cc2abbe8c71c5f2115517f506b0d6706cb780
kingsmo <luthfip20.lp@gmail.com>
2019-11-10 21:05:38 +0700

    update regis js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_lxa0VbbJN',
-        userPoolClientId: '25eavqfibk3joli8t5suc3quil',
-        region: 'us-east-1'
+        userPoolId: 'us-east-2_DYoHQEXzM',
+        userPoolClientId: 'lonb558rf7rqafjqj0ihpeer7',
+        region: 'us-east-2'
     },
     api: {
         invokeUrl: 'https://lk4jug73y1.execute-api.us-east-2.amazonaws.com/api'
