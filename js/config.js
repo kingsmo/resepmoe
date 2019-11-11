b93fb1f56327277b872f35aa1dd13149e0b47916
kingsmo <luthfip20.lp@gmail.com>
2019-11-12 00:23:33 +0700

    update reg

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_DYoHQEXzM',
-        userPoolClientId: 'lonb558rf7rqafjqj0ihpeer7',
+        userPoolId: 'us-east-2_HeVASDJgD',
+        userPoolClientId: '3od2239590go5hp79bguqs582r',
         region: 'us-east-2'
     },
 };
